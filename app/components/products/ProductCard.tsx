d60c3b7b1f1b6fa30a944b714572b6bd91303bc7
Alek30k <alemank23k@gmail.com>
2024-01-28 14:44:47 -0300

    last commit

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,10 +1,23 @@
 "use client";
 
-const ProductCard = () => {
+import Image from "next/image";
+
+interface productCardProps {
+  data: any;
+}
+
+const ProductCard = ({ data }: productCardProps) => {
   return (
     <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
-      <div>
-        <div></div>
+      <div className="flex flex-col items-center w-full gap-1">
+        <div className="aspect-square overflow-hidden relative w-full">
+          <Image
+            src={data.images[0].image}
+            fill
+            className="w-full h-full object-contain"
+            alt={data.name}
+          />
+        </div>
         <div></div>
         <div></div>
         <div></div>
