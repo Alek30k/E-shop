b227c5d7aa20585611d559f2146f012835813ce2
Alek30k <alemank23k@gmail.com>
2024-01-28 16:20:37 -0300

    last commit

---
+++ app/product/ProductDetails.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface ProductDetailsProps {
+  product: any;
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  return <div className="grid grid-cols-1 sm:grid-cols-2">ProductDetails</div>;
+};
+
+export default ProductDetails;
