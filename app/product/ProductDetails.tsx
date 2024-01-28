648de5bae05140a28db684d3e8bd5f0a02f0c68b
Alek30k <alemank23k@gmail.com>
2024-01-28 16:27:00 -0300

    last commit

--- app/product/ProductDetails.tsx
+++ app/product/ProductDetails.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { Rating } from "@mui/material";
+
 interface ProductDetailsProps {
   product: any;
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
-  return <div className="grid grid-cols-1 sm:grid-cols-2">ProductDetails</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div>Images</div>
+      <div>
+        <h2 className="text-3xl font-medium text-slate-700">{product.name}</h2>
+        <div>
+          <Rating />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
