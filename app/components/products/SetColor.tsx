34b01d7cb1af034bc6a48148d83a9cb21a752f90
Alek30k <alemank23k@gmail.com>
2024-01-29 16:08:56 -0300

    last commit

--- app/components/products/SetColor.tsx
+++ app/components/products/SetColor.tsx
@@ -14,14 +14,15 @@ const SetColor = ({
   handleColorSelect,
 }: SetColorProps) => {
   return (
-    <div className="flex gap-4 items-center">
-      <div>
-        <span className="font-semibold">COLOR: </span>
-        <div>
-          {images.map((image, i) => {
+    <div>
+      <div className="flex gap-4 items-center">
+        <span className="font-semibold">COLOR:</span>
+        <div className="flex gap-1">
+          {images.map((image) => {
             return (
               <div
-                key={i}
+                key={image.color}
+                onClick={() => handleColorSelect(image)}
                 className={`h-7 w-7 rounded-full border-teal-300 flex items-center justify-center ${
                   cartProduct.selectedImg.color === image.color
                     ? "border-[1.5px]"
@@ -30,7 +31,7 @@ const SetColor = ({
               >
                 <div
                   style={{ background: image.colorCode }}
-                  className="h-5 w-5 rounded-full"
+                  className="h-5 w-5 rounded-full border-[1.2px] border-slate-300 cursor-pointer"
                 ></div>
               </div>
             );
