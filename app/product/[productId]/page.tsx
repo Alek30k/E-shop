2e7ed881b6f1ed0c4be38b8f20a75417884f568e
Alek30k <alemank23k@gmail.com>
2024-01-30 15:48:01 -0300

    last commit

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -11,6 +11,10 @@ const Product = ({ params }: { params: IParams }) => {
     <div className="p-8">
       <Container>
         <ProductDetails product={product} />
+        <div className="flex flex-col mt-20 gap-4">
+          <div>Add Rating</div>
+          <div>List</div>
+        </div>
       </Container>
     </div>
   );
