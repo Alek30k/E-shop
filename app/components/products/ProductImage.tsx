6d365e2870f009eff0603759c2db55fa40c8f629
Alek30k <alemank23k@gmail.com>
2024-01-30 11:32:14 -0300

    last commit

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -1,6 +1,7 @@
 // "use client";
 
 import { CartProductType, SelecteImgType } from "@/app/product/ProductDetails";
+import Image from "next/image";
 
 interface ProductImgProps {
   cartProduct: CartProductType;
@@ -13,7 +14,25 @@ const ProductImage = ({
   product,
   handleColorSelect,
 }: ProductImgProps) => {
-  return <div>ProductImage</div>;
+  return (
+    <div className="grid grid-cols-6 gap-2 h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+      <div className="flex flex-col items-center justify-center gap-4 cursor-pointer border h-full max-h-[500px] min-h-[300px] sm:min-h-[400px]">
+        {product.images.map((image: SelecteImgType) => {
+          return (
+            <div key={image.color} onClick={() => handleColorSelect(image)}>
+              <Image
+                src={image.image}
+                alt={image.color}
+                fill
+                className="object-contain"
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default ProductImage;
