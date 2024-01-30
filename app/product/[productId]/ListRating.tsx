ae3ce6ea79f9273271c334db7fdc3f4f6a4921ed
Alek30k <alemank23k@gmail.com>
2024-01-30 15:50:29 -0300

    last commit

---
+++ app/product/[productId]/ListRating.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+interface ListRatingProps {
+  product: any;
+}
+
+const ListRating = ({ product }: ListRatingProps) => {
+  return <div>ListRating</div>;
+};
+
+export default ListRating;
