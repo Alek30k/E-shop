fd7f12a2f1333cfd17c8f366377d597a37fb45de
Alek30k <alemank23k@gmail.com>
2024-01-30 11:14:58 -0300

    last commit

--- app/components/products/ProductImage.tsx
+++ app/components/products/ProductImage.tsx
@@ -1,4 +1,12 @@
-"use client";
+// "use client";
+
+import { CartProductType, SelecteImgType } from "@/app/product/ProductDetails";
+
+interface ProductImgProps {
+  cartProduct: CartProductType;
+  product: any;
+  handleColorSelect: (value: SelecteImgType) => void;
+}
 
 const ProductImage = () => {
   return <div>ProductImage</div>;
