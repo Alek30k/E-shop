268bdf5b61aad0d1d277639c9276d444e261752e
Alek30k <alemank23k@gmail.com>
2024-01-30 17:46:32 -0300

    last commit

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import moment from "moment";
 
 interface ListRatingProps {
   product: any;
@@ -10,7 +11,20 @@ const ListRating = ({ product }: ListRatingProps) => {
   return (
     <div>
       <Heading title="Product Review" />
-      <div></div>
+      <div className="text-sm mt-2">
+        {product.reviews &&
+          product.reviews.map((review: any) => {
+            return (
+              <div key={review.id} className="max-w-[300px]">
+                <div>
+                  <div>Avatar</div>
+                  <div>{review?.user.name}</div>
+                  <div>{moment(review.createdDate).fromNow()}</div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
