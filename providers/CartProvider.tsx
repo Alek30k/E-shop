41827da879a908f17ba19daf6d4a11383e432168
Alek30k <alemank23k@gmail.com>
2024-01-31 16:20:51 -0300

    last commit

--- providers/CartProvider.tsx
+++ providers/CartProvider.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { CartContextProvider } from "@/hooks/useCart";
+
 interface CartProviderPops {
   children: React.ReactNode;
 }
 
 const CartProvider = ({ children }: CartProviderPops) => {
-  return <div>CartProvider</div>;
+  return (
+    <div>
+      <CartContextProvider>{children}</CartContextProvider>
+    </div>
+  );
 };
 
 export default CartProvider;
