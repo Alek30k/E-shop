c38143d401a756b492c5e5f784cc15208a1d464c
Alek30k <alemank23k@gmail.com>
2024-01-31 10:40:28 -0300

    last commit

--- app/product/[productId]/ListRating.tsx
+++ app/product/[productId]/ListRating.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import { Rating } from "@mui/material";
 import moment from "moment";
 
 interface ListRatingProps {
@@ -16,10 +17,16 @@ const ListRating = ({ product }: ListRatingProps) => {
           product.reviews.map((review: any) => {
             return (
               <div key={review.id} className="max-w-[300px]">
-                <div>
+                <div className="flex gap-2 items-center">
                   <div>Avatar</div>
-                  <div>{review?.user.name}</div>
-                  <div>{moment(review.createdDate).fromNow()}</div>
+                  <div className="font-semibold">{review?.user.name}</div>
+                  <div className="font-light">
+                    {moment(review.createdDate).fromNow()}
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <Rating value={review.rating} readOnly />
+                  <div>{review.comment}</div>
                 </div>
               </div>
             );
