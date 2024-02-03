5b96160bc889e0739298fe26a6196431d80b9274
Alek30k <alemank23k@gmail.com>
2024-02-03 11:00:15 -0300

    last commit

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -42,14 +42,25 @@ const CartClient = () => {
           })}
       </div>
       <div className="border-t-[1.5px] border-slate-200 py-4 flex justify-between gap-4">
-        <div className="w-[90px]">
-          <Button label="Cleart Cart" onClick={() => {}} small outline />
+        <div className="w-[100px]">
+          <Button label="Clear Cart" onClick={() => {}} small outline />
         </div>
         <div className="text-sm flex flex-col gap-1 items-start">
-          <div>
-            <span>Subtotal</span>
-            <p>Taxes and shipping calculate at checkout</p>
+          <div className="flex justify-between w-full text-base font-semibold">
+            <span className="">Subtotal</span>
+            <span className="">$1000</span>
           </div>
+          <p className="text-slate-500">
+            Taxes and shipping calculate at checkout
+          </p>
+          <Button label="Checkout" onClick={() => {}} />
+          <Link
+            href={"/"}
+            className="text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
+            <span>Start Shopping</span>
+          </Link>
         </div>
       </div>
     </div>
