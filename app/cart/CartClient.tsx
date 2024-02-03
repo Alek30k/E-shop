626bfc0eedc99b6e16c9fa73638368d094fa5ce7
Alek30k <alemank23k@gmail.com>
2024-02-02 22:35:24 -0300

    last commit

--- app/cart/CartClient.tsx
+++ app/cart/CartClient.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import { useCart } from "@/hooks/useCart";
 import Link from "next/link";
+import { MdArrowBack } from "react-icons/md";
+import Heading from "../components/Heading";
+import Button from "../components/products/Button";
 
 const CartClient = () => {
   const { cartProducts } = useCart();
 
   if (!cartProducts || cartProducts.length === 0) {
     return (
-      <div>
-        <div>Your cart is empty</div>
+      <div className="flex flex-col items-center">
+        <div className="text-2xl">Your cart is empty</div>
         <div>
-          <Link href={"/"}>
+          <Link
+            href={"/"}
+            className="text-slate-500 flex items-center gap-1 mt-2"
+          >
+            <MdArrowBack />
             <span>Start Shopping</span>
           </Link>
         </div>
@@ -19,7 +28,24 @@ const CartClient = () => {
 
   return (
     <div>
-      <h1>CartClientasd</h1>
+      <Heading title="Shopping Cart" center />
+      <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center mt-8">
+        <div className="col-span-2 justify-self-start">PRODUCT</div>
+        <div className="justify-self-center">PRICE</div>
+        <div className="justify-self-center">QUANTITY</div>
+        <div className="justify-self-end">TOTAL</div>
+      </div>
+      <div>
+        {cartProducts &&
+          cartProducts.map((item) => {
+            return <div key={item.id}>{item.name}</div>;
+          })}
+      </div>
+      <div>
+        <div>
+          <Button label="Cleart Cart" onClick={() => {}} />
+        </div>
+      </div>
     </div>
   );
 };
