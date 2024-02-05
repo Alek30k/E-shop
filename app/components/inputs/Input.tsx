99c0755ce6cd175c697d92682fe205f199e9cd13
Alek30k <alemank23k@gmail.com>
2024-02-05 16:01:19 -0300

    last commit

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,4 +1,26 @@
-const Input = () => {
+"use client";
+
+import { FieldError, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  id: string;
+  label: string;
+  type?: string;
+  disabled: boolean;
+  required?: boolean;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldError;
+}
+
+const Input = ({
+  id,
+  label,
+  type,
+  disabled,
+  required,
+  register,
+  errors,
+}: InputProps) => {
   return <div>Input</div>;
 };
 
