ec83c157876a3b54886b8a923d3a975c8931501f
Alek30k <alemank23k@gmail.com>
2024-02-06 16:39:14 -0300

    last commit

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -6,7 +6,7 @@ interface InputProps {
   id: string;
   label: string;
   type?: string;
-  disabled: boolean;
+  disabled?: boolean;
   required?: boolean;
   register: UseFormRegister<FieldValues>;
   errors: FieldErrors;
@@ -37,7 +37,7 @@ const Input = ({
       />
       <label
         htmlFor={id}
-        className="absolute cursor-text text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown: scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus-translate-y-4"
+        className="absolute cursor-text text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
       >
         {label}
       </label>
--- app/register/RegisterForm.tsx
+++ app/register/RegisterForm.tsx
@@ -3,14 +3,36 @@
 import { useState } from "react";
 import Heading from "../components/Heading";
 import Input from "../components/inputs/Input";
+import { register } from "module";
+import { error } from "console";
+import { FieldValues, useForm, FormSubmitHandler } from "react-hook-form";
 
 const RegisterForm = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
   return (
     <>
       <Heading title="Sign up for E-Shop" />
       <hr className="bg-slate-300 w-full h-px" />
-      <Input id="name" label="Name" disabled={isLoading} />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </>
   );
 };
