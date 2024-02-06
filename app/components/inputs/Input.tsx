23c50505bc73dfb483719de57f1443f814239228
Alek30k <alemank23k@gmail.com>
2024-02-06 14:38:50 -0300

    last commit

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -21,7 +21,23 @@ const Input = ({
   register,
   errors,
 }: InputProps) => {
-  return <div>Input</div>;
+  return (
+    <div className="relative w-full">
+      <input
+        autoComplete="off"
+        id={id}
+        disabled={disabled}
+        {...register(id, { required })}
+        placeholder=""
+        type={type}
+        className={`peer w-full p-4 pt-6 outline-none bg-white font-light border-2 rounded-md transition disabled:opacity-70 disabled:cursor-not-allowed 
+      ${errors[id] ? "border-rose-400" : "border-slate-300"}
+      ${errors[id] ? "focus:border-rose-400" : "focus:border-slate-300"}
+      `}
+      />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
 };
 
 export default Input;
