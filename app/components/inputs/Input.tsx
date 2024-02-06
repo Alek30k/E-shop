37fa9a91a83b67f02dc957f039b120fbe2bbfed7
Alek30k <alemank23k@gmail.com>
2024-02-06 15:23:27 -0300

    last commit

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -35,7 +35,12 @@ const Input = ({
       ${errors[id] ? "focus:border-rose-400" : "focus:border-slate-300"}
       `}
       />
-      <label htmlFor={id}>{label}</label>
+      <label
+        htmlFor={id}
+        className="absolute cursor-text text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] left-4 peer-placeholder-shown: scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus-translate-y-4"
+      >
+        {label}
+      </label>
     </div>
   );
 };
