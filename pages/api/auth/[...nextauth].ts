6a327870c822c2635b50b59eb677e0ff4d87172f
Alek30k <alemank23k@gmail.com>
2024-02-08 15:22:57 -0300

    last commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,42 +13,48 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     CredentialsProvider({
-        name:"credentials",
-        credentials:{
-            email:{
-                label:"email",
-                type:"text",
-            }
-            password:{
-                label:"password",
-                type:"password",
-            }
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
         },
-        async authorize(credentials){
-            if(!credentials?.email || !credentials?.password) {
-                throw new Error("Invalid email or password")
-            }
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: credentials.email
-                }
-            })
-            if(!user || !user.hashedPassword){
-                throw new Error("Invalid email or password")
-            }
-
-            const isCorrectPassword = await bcrypt.compare(
-                credentials.password,
-                user.password
-            )
-            if(!isCorrectPassword){
-                throw new Error("Invalid email or password")
-            }
-
-            return user
+        password: {
+          label: "password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid email or password");
         }
-            
-        
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!isCorrectPassword) {
+          throw new Error("Invalid email or password");
+        }
+
+        return user;
+      },
     }),
   ],
+  pages: {
+    signIn: "/login",
+  },
+  debug: process.env.NODE_ENV !== "development",
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 });
