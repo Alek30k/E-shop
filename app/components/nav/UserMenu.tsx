b3d5c76010845dc31e5c33bc0ea2308409251afc
Alek30k <alemank23k@gmail.com>
2024-02-09 16:17:10 -0300

    last commit

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -1,18 +1,26 @@
 "use client";
 
 import { useCallback, useState } from "react";
+import Avatar from "../Avatar";
+import { AiFillCaretDown } from "react-icons/ai";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
-    setIsOpen(true);
+    setIsOpen((prev) => !prev);
   }, []);
 
   return (
     <>
       <div className="relative z-30">
-        <div onClick={toggleOpen}></div>
+        <div
+          onClick={toggleOpen}
+          className="p-2 border-[1px] border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer haver:shadow-md transition text-slate-700"
+        >
+          <Avatar />
+          <AiFillCaretDown />
+        </div>
       </div>
     </>
   );
