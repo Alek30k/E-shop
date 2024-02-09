6fe470b12c2049bf9d042e3a2b2c6e40d3f864d5
Alek30k <alemank23k@gmail.com>
2024-02-09 16:39:01 -0300

    last commit

--- app/components/nav/MenuItem.tsx
+++ app/components/nav/MenuItem.tsx
@@ -4,7 +4,14 @@ interface MenuItemProps {
 }
 
 const MenuItem = ({ children, onClick }: MenuItemProps) => {
-  return <div onClick={onClick}>{children}</div>;
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 hover:bg-neutral-100 transition"
+    >
+      {children}
+    </div>
+  );
 };
 
 export default MenuItem;
--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -3,6 +3,8 @@
 import { useCallback, useState } from "react";
 import Avatar from "../Avatar";
 import { AiFillCaretDown } from "react-icons/ai";
+import Link from "next/link";
+import MenuItem from "./MenuItem";
 
 const UserMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,7 +24,13 @@ const UserMenu = () => {
           <AiFillCaretDown />
         </div>
         {isOpen && (
-          <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer"></div>
+          <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer">
+            <div>
+              <Link href="orders">
+                <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
+              </Link>
+            </div>
+          </div>
         )}
       </div>
     </>
