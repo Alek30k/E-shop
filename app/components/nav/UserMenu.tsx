cad44eea037fc0a3fded867dc520381060c880d2
Alek30k <alemank23k@gmail.com>
2024-02-09 22:46:45 -0300

    last commit

--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -27,7 +27,7 @@ const UserMenu = () => {
           <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer">
             <div>
               <Link href="orders">
-                <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
+                <MenuItem onClick={toggleOpen}>Your Orderss</MenuItem>
               </Link>
             </div>
           </div>
