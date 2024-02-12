2d30c11ad08d720833258878c600ec734216edad
Alek30k <alemank23k@gmail.com>
2024-02-12 15:27:06 -0300

    last commit

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/libs/prismadb";
 import { getServerSession } from "next-auth";
 
 export async function getSession() {
@@ -12,6 +13,23 @@ export async function getCurrentUser() {
     if (!session?.user?.email) {
       return null;
     }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session?.user?.email,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return {
+      ...currentUser,
+      createdAt: currentUser.createdAt?.toISOString(),
+      updatedAt: currentUser.updatedAt?.toISOString(),
+      emailVerified: currentUser.emailVerified?.toString() || null,
+    };
   } catch (error: any) {
     return null;
   }
