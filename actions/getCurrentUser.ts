c260a06176c0ffff2f7df4cf38d78c4f36143583
Alek30k <alemank23k@gmail.com>
2024-02-12 15:18:28 -0300

    last commit

---
+++ actions/getCurrentUser.ts
@@ -0,0 +1,18 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+  } catch (error: any) {
+    return null;
+  }
+}
