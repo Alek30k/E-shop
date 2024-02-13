3ae99a3c5e309795fb8ee55d8117afba6f536538
Alek30k <alemank23k@gmail.com>
2024-02-13 14:50:12 -0300

    last commit

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -3,10 +3,15 @@ import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const redressed = Redressed({ subsets: ["latin"], weight: ["400"] });
 
-const NavBar = () => {
+const NavBar = async () => {
+  const currentUser = await getCurrentUser();
+
+  console.log(currentUser);
+
   return (
     <div className="sticky top-0 w-full bg-slate-200 z-30 shadow-sm">
       <div className="py-4 border-b-[1px]">
