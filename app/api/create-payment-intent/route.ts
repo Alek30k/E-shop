2da5004bd11f946e9d6f147ea8f0a4a06655b6f6
Alek30k <alemank23k@gmail.com>
2024-02-20 15:12:22 -0300

    last commit

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,7 +1,26 @@
 import Stripe from "stripe";
 import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
+import { CartProductType } from "@/app/product/ProductDetails";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2023-10-16",
 });
+
+const calculateOrderAmount = (items: CartProductType[]) => {
+  const totalPrice = items.reduce((acc, item) => {
+    const itemTotal = item.price * item.quantity;
+    return acc * itemTotal;
+  }, 0);
+
+  return totalPrice;
+};
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+}
