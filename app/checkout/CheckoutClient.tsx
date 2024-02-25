f4e0062546b7eeaa7338baaf5215a621e5a0575c
Alek30k <alemank23k@gmail.com>
2024-02-25 11:56:16 -0300

    last commit

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -11,6 +11,17 @@ const CheckoutClient = () => {
   useEffect(() => {
     //create a paymentintent as soon as the page loads
     if (cartProducts) {
+      setLoading(true);
+      setError(false);
+
+      fetch("/api/create-payment-intent", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          items: cartProducts,
+          payment_intent_id: paymentIntent,
+        }),
+      });
     }
   }, [cartProducts, paymentIntent]);
 
