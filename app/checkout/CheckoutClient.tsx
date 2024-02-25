536c42555fb84ec9a0e7ccca153d910798b448b5
Alek30k <alemank23k@gmail.com>
2024-02-25 11:49:37 -0300

    last commit

--- app/checkout/CheckoutClient.tsx
+++ app/checkout/CheckoutClient.tsx
@@ -1,5 +1,20 @@
+"use client";
+
+import { useCart } from "@/hooks/useCart";
+import { useEffect, useState } from "react";
+
 const CheckoutClient = () => {
-  return <div className="p-8">CheckoutClient</div>;
+  const { cartProducts, paymentIntent, handleSetPaymentIntent } = useCart();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    //create a paymentintent as soon as the page loads
+    if (cartProducts) {
+    }
+  }, [cartProducts, paymentIntent]);
+
+  return <>Checkout</>;
 };
 
 export default CheckoutClient;
