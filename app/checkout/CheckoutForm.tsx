49ea9612543e255d7c747e2f84c1e3c52344f0f4
Alek30k <alemank23k@gmail.com>
2024-02-27 15:23:14 -0300

    last commit

--- app/checkout/CheckoutForm.tsx
+++ app/checkout/CheckoutForm.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useCart } from "@/hooks/useCart";
+import { useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+
 interface CheckoutFormProps {
   clientSecret: string;
   handleSetPaymentSuccess: (value: boolean) => void;
@@ -9,6 +13,12 @@ const CheckoutForm = ({
   clientSecret,
   handleSetPaymentSuccess,
 }: CheckoutFormProps) => {
+  const { cartTotalAmount, handleClearCart, handleSetPaymentIntent } =
+    useCart();
+  const stripe = useStripe();
+  const elements = useElements();
+  const [isLoading, setIsLoading] = useState(false);
+
   return <div>CheckoutForm</div>;
 };
 
