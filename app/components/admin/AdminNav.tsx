0aa79c1251591c68e1cb8cd2b1354e8781ae8535
Alek30k <alemank23k@gmail.com>
2024-02-29 14:25:25 -0300

    last commit

--- app/components/admin/AdminNav.tsx
+++ app/components/admin/AdminNav.tsx
@@ -1,10 +1,24 @@
+import Link from "next/link";
 import Container from "../Container";
+import AdminNavItem from "./AdminNavItem";
+import { MdDashboard } from "react-icons/md";
+import { usePathname } from "next/navigation";
 
 const AdminNav = () => {
+  const pathName = usePathname();
+
   return (
     <div className="w-full shadow-sm top-20 border-b-[1px] pt-4">
       <Container>
-        <div className="flex flex-row items-center justify-between md:justify-center  md:gap-12"></div>
+        <div className="flex flex-row items-center justify-between md:justify-center  md:gap-12 overflow-auto flex-nowrap">
+          <Link href="/admin">
+            <AdminNavItem
+              label="Summary"
+              icon={MdDashboard}
+              selected={pathName === "/admin"}
+            />
+          </Link>
+        </div>
       </Container>
     </div>
   );
