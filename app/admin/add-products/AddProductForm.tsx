9318bf49f9c32ab747043a168231836ea3a462b1
Alek30k <alemank23k@gmail.com>
2024-03-01 16:40:58 -0300

    last commit

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -2,13 +2,21 @@
 
 import Heading from "@/app/components/Heading";
 import Input from "@/app/components/inputs/Input";
+import TextArea from "@/app/components/inputs/TextArea";
 import { register } from "module";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
 const AddProductForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const {} = useForm<FieldValues>({
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
     defaultValues: {
       name: "",
       description: "",
@@ -25,6 +33,31 @@ const AddProductForm = () => {
       <Input
         id="name"
         label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="price"
+        label="Price"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        type="number"
+        required
+      />
+      <Input
+        id="brand"
+        label="Brand"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <TextArea
+        id="description"
+        label="Description"
         disabled={isLoading}
         register={register}
         errors={errors}
