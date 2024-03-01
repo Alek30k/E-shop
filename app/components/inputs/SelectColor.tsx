d1168b5c25ab4d3c1992160b44b48bbbbd054382
Alek30k <alemank23k@gmail.com>
2024-03-01 18:15:05 -0300

    last commit

--- app/components/inputs/SelectColor.tsx
+++ app/components/inputs/SelectColor.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { ImageType } from "@/app/admin/add-products/AddProductForm";
+import { useEffect, useState } from "react";
 
 interface SelectColorProps {
   item: ImageType;
   addImageToState: (value: ImageType) => void;
   removeImageFromState: (value: ImageType) => void;
+  isProductCreated: boolean;
 }
 
-const SelectColor = ({}: SelectColorProps) => {
+const SelectColor = ({
+  item,
+  addImageToState,
+  removeImageFromState,
+  isProductCreated,
+}: SelectColorProps) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    if (isProductCreated) {
+      setIsSelected(false);
+      setFile(null);
+    }
+  }, [isProductCreated]);
+
   return <div>SelectColor</div>;
 };
 
