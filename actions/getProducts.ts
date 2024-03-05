51eee0b1121676bc2e1eca00e1349516475d8576
Alek30k <alemank23k@gmail.com>
2024-03-05 11:26:17 -0300

    last commit

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -41,6 +41,9 @@ export default async function getProducts(params: IProducParams) {
           include: {
             user: true,
           },
+          orderBy: {
+            createdDate: "desc",
+          },
         },
       },
     });
