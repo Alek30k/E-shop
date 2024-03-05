721d86a0bd87bdd9956d47daac1acf5fd0861438
Alek30k <alemank23k@gmail.com>
2024-03-05 11:04:57 -0300

    last commit

--- actions/getProducts.ts
+++ actions/getProducts.ts
@@ -1,1 +1,32 @@
 import prisma from "@/libs/prismadb";
+
+export interface IProducParams {
+  category?: string | null;
+  searchTerm?: string | null;
+}
+
+export default async function getProducts(params: IProducParams) {
+  try {
+    const { category, searchTerm } = params;
+    let searchString = searchTerm;
+
+    if (!searchTerm) {
+      searchString = "";
+    }
+
+    let query: any = {};
+
+    if (category) {
+      query.category = category;
+    }
+
+    const products = await prisma.product.findMany({
+      where: {
+        ...query,
+        OR: [{}],
+      },
+    });
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
