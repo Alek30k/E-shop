fa602be3be1ec3e1bf0531fc3b0c5a621740d8e9
Alek30k <alemank23k@gmail.com>
2024-03-05 16:18:51 -0300

    last commit

--- actions/getOrders.ts
+++ actions/getOrders.ts
@@ -2,5 +2,16 @@ import prisma from "@/libs/prismadb";
 
 export default async function getOrders() {
   try {
-  } catch (error) {}
+    const orders = await prisma.order.findMany({
+      include: {
+        user: true,
+      },
+      orderBy: {
+        createDate: "desc",
+      },
+    });
+    return orders;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
