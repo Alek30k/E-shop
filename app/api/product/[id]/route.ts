f07840cf9c06ddd4425953a9919835dda238828b
Alek30k <alemank23k@gmail.com>
2024-03-06 16:48:10 -0300

    last commit

--- app/api/product/[id]/route.ts
+++ app/api/product/[id]/route.ts
@@ -11,8 +11,6 @@ export async function DELETE(
   if (!currentUser || currentUser.role !== "ADMIN") {
     return NextResponse.error();
   }
-  const body = await request.json();
-  const { id, inStock } = body;
 
   const product = await prisma.product.delete({
     where: { id: params.id },
