77e327a43fd92f4f61b742a1073ddb84e3f60dd9
Alek30k <alemank23k@gmail.com>
2024-03-07 16:31:29 -0300

    last commit

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -80,7 +80,10 @@ const OrderDetails = ({ order }: OrderDetailsProps) => {
       <div>
         <h2 className="font-semibold mt-4 mb-2">Products ordered</h2>
         <div className="grid grid-cols-5 text-xs gap-4 pb-2 items-center">
-          <Heading />
+          <div className="cols-span-2 justify-self-start">PRODUCT</div>
+          <div className="justify-self-center">PRICE</div>
+          <div className="justify-self-center">QTY</div>
+          <div className="justify-self-end">TOTAL</div>
         </div>
       </div>
     </div>
