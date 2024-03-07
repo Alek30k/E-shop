ba79e75f2c3a87062c8b2a7e99ced6df73dfd820
Alek30k <alemank23k@gmail.com>
2024-03-07 15:04:18 -0300

    last commit

--- app/order/[orderId]/OrderDetails.tsx
+++ app/order/[orderId]/OrderDetails.tsx
@@ -5,7 +5,7 @@ import Status from "@/app/components/Status";
 import { formatPrice } from "@/utils/formatPrice";
 import { Order } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import { MdAccessTimeFilled, MdDone } from "react-icons/md";
+import { MdAccessTimeFilled, MdDeliveryDining, MdDone } from "react-icons/md";
 
 interface OrderDetailsProps {
   order: Order;
@@ -24,7 +24,7 @@ const OrderDetails = ({ order }: OrderDetailsProps) => {
         Total amount{" "}
         <span className="font-bold">{formatPrice(order.amount)}</span>
       </div>
-      <div>
+      <div className="flex gap-2 items-center">
         <div>Payment status:</div>
         <div>
           {order.status === "pending" ? (
@@ -37,6 +37,35 @@ const OrderDetails = ({ order }: OrderDetailsProps) => {
           ) : order.status === "complete" ? (
             <Status
               text="completed"
+              icon={MdDone}
+              bg="bg-green-200"
+              color="text-green-700"
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+      <div className="flex gap-2 items-center">
+        <div>Delivery status:</div>
+        <div>
+          {order.deliveryStatus === "pending" ? (
+            <Status
+              text="pending"
+              icon={MdAccessTimeFilled}
+              bg="bg-slate-200"
+              color="text-slate-700"
+            />
+          ) : order.deliveryStatus === "dispatched" ? (
+            <Status
+              text="dispatched"
+              icon={MdDeliveryDining}
+              bg="bg-purple-200"
+              color="text-purple-700"
+            />
+          ) : order.deliveryStatus === "delivered" ? (
+            <Status
+              text="delivered"
               icon={MdDone}
               bg="bg-green-200"
               color="text-green-700"
