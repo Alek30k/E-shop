88c9a917f58dd6f19f7909479b1f5bbcd2469c9c
Alek30k <alemank23k@gmail.com>
2024-03-07 22:29:23 -0300

    last commit

---
+++ app/orders/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/app/components/Container";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+import getOrdersByUserId from "@/actions/getOrdersByUserId";
+import ManageOrdersClient from "../admin/manage-orders/ManageOrdersClient";
+
+const Orders = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return <NullData title="Oops! Access denied" />;
+  }
+
+  const orders = await getOrdersByUserId(currentUser.id);
+
+  return (
+    <div className="pt-8">
+      <Container>
+        <ManageOrdersClient orders={orders} />
+      </Container>
+    </div>
+  );
+};
+
+export default Orders;
