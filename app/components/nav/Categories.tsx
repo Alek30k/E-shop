5f094159c70da5f19b2a510ae39c3a17e246d5ee
Alek30k <alemank23k@gmail.com>
2024-03-08 15:33:25 -0300

    last commit

---
+++ app/components/nav/Categories.tsx
@@ -0,0 +1,13 @@
+import Container from "../Container";
+
+function Categories() {
+  return (
+    <div className="bg-white">
+      <Container>
+        <div></div>
+      </Container>
+    </div>
+  );
+}
+
+export default Categories;
