0f6c0863295033d1883bf96231cfbd4ca0589f3a
Alek30k <alemank23k@gmail.com>
2024-03-08 16:45:46 -0300

    last commit

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -1,4 +1,5 @@
 import { getCurrentUser } from "@/actions/getCurrentUser";
+import { Review } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
@@ -9,4 +10,13 @@ export async function POST(request: Request) {
   }
   const body = await request.json();
   const { comment, rating, product, userId } = body;
+
+  const deliveredOrder = currentUser?.orders.some(
+    (order) =>
+      order.products.find((item) => item.id === product.id) &&
+      order.deliveryStatus === "delivered"
+  );
+  const userReview = product?.reviews.find((review: Review) => {
+    return review.userId === currentUser.id;
+  });
 }
