2d19c549ae8b7f5977a0c8764285685a19adc40b
Alek30k <alemank23k@gmail.com>
2024-03-08 15:55:29 -0300

    last commit

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -1,10 +1,15 @@
+import { categories } from "@/utils/Categories";
 import Container from "../Container";
 
 function Categories() {
   return (
     <div className="bg-white">
       <Container>
-        <div></div>
+        <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+          {categories.map((item) => (
+            <div key={item.id}></div>
+          ))}
+        </div>
       </Container>
     </div>
   );
