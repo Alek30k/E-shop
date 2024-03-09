faf82aaf3223a16262d2248878740c9fecb5eac7
Alek30k <alemank23k@gmail.com>
2024-03-08 23:24:54 -0300

    last commit

--- app/product/[productId]/AddRating.tsx
+++ app/product/[productId]/AddRating.tsx
@@ -2,7 +2,9 @@
 
 import { SafeUser } from "@/types";
 import { Order, Product, Review } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 interface AddRatingProps {
   product: Product & {
@@ -17,6 +19,30 @@ interface AddRatingProps {
 
 const AddRating = ({ product, user }: AddRatingProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    reset,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      comment: "",
+      rating: 0,
+    },
+  });
+
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldTouch: true,
+      shouldDirty: true,
+      shouldValidate: true,
+    });
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {};
 
   return <div></div>;
 };
