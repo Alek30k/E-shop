2151a3bbdae1c67941463398945e412307f6dd2e
Alek30k <alemank23k@gmail.com>
2024-03-09 16:10:14 -0300

    last commit

--- app/components/nav/SearchBar.tsx
+++ app/components/nav/SearchBar.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+import queryString from "query-string";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const SearchBar = () => {
+  const router = useRouter();
+
   const {
     register,
     handleSubmit,
@@ -14,7 +18,22 @@ const SearchBar = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<FieldValues> = async (data) => {};
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    if (!data.searchTerm) return router.push("/");
+
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: {
+          searchTerm: data.searchTerm,
+        },
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+    reset();
+  };
 
   return (
     <div className="flex items-center">
@@ -25,7 +44,10 @@ const SearchBar = () => {
         placeholder="Explore E-Shop"
         className="p-2 border border-gray-300 rounded-l-md focus:outline-none focus:border-[0.5px] focus:border-slate-500 w-80"
       />
-      <button className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md">
+      <button
+        onClick={handleSubmit(onSubmit)}
+        className="bg-slate-700 hover:opacity-80 text-white p-2 rounded-r-md"
+      >
         Search
       </button>
     </div>
