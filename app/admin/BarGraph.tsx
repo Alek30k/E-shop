a2163ac06dffc7e69b045aeda541c21bdd6b817d
Alek30k <alemank23k@gmail.com>
2024-03-10 12:30:07 -0300

    last commit

--- app/admin/BarGraph.tsx
+++ app/admin/BarGraph.tsx
@@ -1,17 +1,53 @@
-"use client"
+"use client";
 
-import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend} from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
-<Doughnut data={...} />
-
-
-const BarGraph = () => {
-  return (
-    <>BarGraph</>
-  )
+interface BarGraphProps {
+  data: GraphData[];
 }
 
-export default BarGraph
+type GraphData = {
+  day: string;
+  date: string;
+  totalAmount: number;
+};
+
+const BarGraph = ({ data }: BarGraphProps) => {
+  const labels = data.map((item) => item.day);
+  const amounts = data.map((item) => item.totalAmount);
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Sale Amount",
+        data: amounts,
+        backgroundColor: "rgba(75, 192, 192, 0.6)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={chartData} options={options}></Bar>;
+};
+
+export default BarGraph;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,8 @@ import Summary from "./Summary";
 import getOrders from "@/actions/getOrders";
 import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
+import BarGraph from "./BarGraph";
+import getGraphData from "@/actions/getGraphData";
 
 export const metadata = {
   title: "E-shop Admin",
@@ -13,11 +15,15 @@ const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
+  const graphData = await getGraphData();
 
   return (
     <div className="pt-8 ">
       <Container>
         <Summary orders={orders} products={products} users={users} />
+        <div className="mt-4 mx-auto max-w-[1150px]">
+          <BarGraph data={graphData} />
+        </div>
       </Container>
     </div>
   );
