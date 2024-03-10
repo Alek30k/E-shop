95bb086fa5cb4ef9db261db864578942f5a4a6f0
Alek30k <alemank23k@gmail.com>
2024-03-09 22:24:52 -0300

    last commit

--- app/admin/Summary.tsx
+++ app/admin/Summary.tsx
@@ -2,6 +2,7 @@
 
 import { Order, Product, User } from "@prisma/client";
 import { useEffect, useState } from "react";
+import Heading from "../components/Heading";
 
 interface SumaryProps {
   orders: Order[];
@@ -73,7 +74,30 @@ const Summary = ({ orders, products, users }: SumaryProps) => {
     });
   }, [orders, products, users]);
 
-  return <div></div>;
+  const summaryKeys = Object.keys(summaryData);
+
+  return (
+    <div className="mx-w-[1150px m-auto]">
+      <div className="mb-4 mt-8">
+        <Heading title="Stats" />
+      </div>
+      <div className="grid grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+        {summaryKeys &&
+          summaryKeys.map((key) => {
+            return (
+              <div
+                key={key}
+                className="rounded-xl border-2 p-4 flex flex-col items-center gap-2 transition"
+              >
+                <div className="text-xl md:text-4xl font-bold">
+                  {summaryData[key].label === "Total Sale"}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default Summary;
