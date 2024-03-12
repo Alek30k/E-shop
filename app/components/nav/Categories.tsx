2ac0cb08e2848a0f07183277ab80a27f966df2f9
Alek30k <alemank23k@gmail.com>
2024-03-12 19:20:01 -0300

    last commit

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -4,6 +4,7 @@ import { categories } from "@/utils/Categories";
 import Container from "../Container";
 import Category from "./Category";
 import { usePathname, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function Categories() {
   const params = useSearchParams();
@@ -20,19 +21,21 @@ function Categories() {
   return (
     <div className="bg-white">
       <Container>
-        <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-          {categories.map((item) => (
-            <Category
-              key={item.label}
-              label={item.label}
-              icon={item.icon}
-              selected={
-                category === item.label ||
-                (category === null && item.label === "All")
-              }
-            />
-          ))}
-        </div>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+            {categories.map((item) => (
+              <Category
+                key={item.label}
+                label={item.label}
+                icon={item.icon}
+                selected={
+                  category === item.label ||
+                  (category === null && item.label === "All")
+                }
+              />
+            ))}
+          </div>
+        </Suspense>
       </Container>
     </div>
   );
