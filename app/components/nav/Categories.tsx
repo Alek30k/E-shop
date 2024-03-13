617a926698e12028b6c4cdb616d3e38498f64c31
Alek30k <alemank23k@gmail.com>
2024-03-13 18:31:15 -0300

    last commit

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -3,12 +3,11 @@
 import { categories } from "@/utils/Categories";
 import Container from "../Container";
 import Category from "./Category";
-import { usePathname } from "next/navigation";
-import { Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
 function Categories() {
-  // const params = useSearchParams();
-  // const category = params?.get("category");
+  const params = useSearchParams();
+  const category = params?.get("category");
   const pathname = usePathname();
 
   const isMainPage = pathname === "/";
@@ -21,21 +20,19 @@ function Categories() {
   return (
     <div className="bg-white dark:bg-darkcategory ">
       <Container>
-        <Suspense fallback={<div>Cargando...</div>}>
-          <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto ">
-            {categories.map((item) => (
-              <Category
-                key={item.label}
-                label={item.label}
-                icon={item.icon}
-                // selected={
-                //   category === item.label ||
-                //   (category === null && item.label === "All")
-                // }
-              />
-            ))}
-          </div>
-        </Suspense>
+        <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto ">
+          {categories.map((item) => (
+            <Category
+              key={item.label}
+              label={item.label}
+              icon={item.icon}
+              selected={
+                category === item.label ||
+                (category === null && item.label === "All")
+              }
+            />
+          ))}
+        </div>
       </Container>
     </div>
   );
