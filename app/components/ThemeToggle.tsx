890b594d108c5f2ffba54f932a60b20433a20c83
Alek30k <alemank23k@gmail.com>
2024-03-14 14:28:29 -0300

    last commit

--- app/components/ThemeToggle.tsx
+++ app/components/ThemeToggle.tsx
@@ -3,38 +3,59 @@
 import { useEffect, useState } from "react";
 import { FaMoon } from "react-icons/fa";
 import { BsSunFill } from "react-icons/bs";
+import { FiSun, FiMoon } from "react-icons/fi";
+import { useTheme } from "next-themes";
+import Image from "next/image";
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(true);
+  // const [darkMode, setDarkMode] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (theme === "dark") setDarkMode(true);
-  }, []);
+  useEffect(() => setMounted(true), []);
 
-  useEffect(() => {
-    if (darkMode) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-      localStorage.setItem("theme", "light");
-    }
-  }, [darkMode]);
+  if (!mounted)
+    return (
+      <Image
+        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
+        width={36}
+        height={36}
+        sizes="36x36"
+        alt="Loading Light/Dark Toggle"
+        priority={false}
+        title="Loading Light/Dark Toggle"
+      />
+    );
 
-  return (
-    <div
-      className="relative w-16 h-8 flex items-center dark:bg-gray-900 bg-teal-500 cursor-pointer rounded-full p-1"
-      onClick={() => setDarkMode(!darkMode)}
-    >
-      <FaMoon className="text-white" size={18} />
-      <div
-        className="absolute bg-white dark:bg-medium w-6 h-6 rounded-full shadow-md transform transition-transform duration-300"
-        style={darkMode ? { left: "2px" } : { right: "2px" }}
-      ></div>
-      <BsSunFill className="ml-auto text-yellow-400" size={18} />
-    </div>
-  );
+  // useEffect(() => {
+  //   const theme = localStorage.getItem("theme");
+  //   if (theme === "dark") setDarkMode(true);
+  // }, []);
+
+  // useEffect(() => {
+  //   if (darkMode) {
+  //     document.documentElement.classList.add("dark");
+  //     localStorage.setItem("theme", "dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //     localStorage.setItem("theme", "light");
+  //   }
+  // }, [darkMode]);
+  if (resolvedTheme === "dark") {
+    return (
+      <div className="text-3xl">
+        <FiSun onClick={() => setTheme("light")} />
+      </div>
+    );
+  }
+
+  if (resolvedTheme === "light") {
+    return (
+      <div className="text-3xl">
+        <FiMoon onClick={() => setTheme("dark")} />
+      </div>
+    );
+  }
 };
 
 export default ThemeToggle;
--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -6,8 +6,8 @@ import Category from "./Category";
 import { usePathname, useSearchParams } from "next/navigation";
 
 function Categories() {
-  // const params = useSearchParams();
-  // const category = params?.get("category");
+  const params = useSearchParams();
+  const category = params?.get("category");
   const pathname = usePathname();
 
   const isMainPage = pathname === "/";
@@ -23,10 +23,10 @@ function Categories() {
               key={item.label}
               label={item.label}
               icon={item.icon}
-              // selected={
-              //   category === item.label ||
-              //   (category === null && item.label === "All")
-              // }
+              selected={
+                category === item.label ||
+                (category === null && item.label === "All")
+              }
             />
           ))}
         </div>
