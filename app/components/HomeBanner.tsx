ace913284f79220696852d12c06304b1a2745bad
Alek30k <alemank23k@gmail.com>
2024-03-16 17:23:04 -0300

    last commit

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -1,30 +1,50 @@
-import Image from "next/image";
+"use client";
 
-const HomeBanner = () => {
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Skeleton from "../skeletons/skeletonBanner";
+
+interface homeBannerProps {
+  data: any;
+}
+
+const HomeBanner = ({ data }: homeBannerProps) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data) setLoading(false);
+  }, []);
+
   return (
-    <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
-      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
-        <div className="mb-8 md:mb-0 text-center">
-          <h1 className="text-4xl md:text-6xl font-bold text-white">
-            Summer Sale!
-          </h1>
-          <p className="text-lg md:text-xl text-white mb-2">
-            Enjoy discouts on selected items
-          </p>
-          <p className="text-2xl md:text-5xl text-yellow-400 font-bold">
-            GET 50% OFF
-          </p>
+    <>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
+          <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
+            <div className="mb-8 md:mb-0 text-center">
+              <h1 className="text-4xl md:text-6xl font-bold text-white">
+                Summer Sale!
+              </h1>
+              <p className="text-lg md:text-xl text-white mb-2">
+                Enjoy discouts on selected items
+              </p>
+              <p className="text-2xl md:text-5xl text-yellow-400 font-bold">
+                GET 50% OFF
+              </p>
+            </div>
+            <div className="w-1/3 relative aspect-video">
+              <Image
+                src="/banner-image.png"
+                fill
+                alt="Banner Image"
+                className="object-contain"
+              />
+            </div>
+          </div>
         </div>
-        <div className="w-1/3 relative aspect-video">
-          <Image
-            src="/banner-image.png"
-            fill
-            alt="Banner Image"
-            className="object-contain"
-          />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Skeleton from "@/app/skeletons/skeleton";
 import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 interface productCardProps {
   data: any;
@@ -12,34 +14,49 @@ interface productCardProps {
 
 const ProductCard = ({ data }: productCardProps) => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (data) setLoading(false);
+  }, []);
 
   const productRaiting =
     data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     data.reviews.length;
 
   return (
-    <div
-      onClick={() => router.push(`/product/${data.id}`)}
-      className="col-span-1 cursor-pointer border-[1.2px] border-slate-200  bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm"
-    >
-      <div className="flex flex-col items-center w-full gap-1 dark:text-medium">
-        <div className="aspect-square overflow-hidden relative w-full">
-          <Image
-            src={data.images[0].image}
-            fill
-            className="w-full h-full object-contain"
-            alt={data.name}
-          />
-        </div>
-        <hr className="w-[100%] mt-6" />
-        <div className="mt-4">{truncateText(data.name)}</div>
-        <div>
-          <Rating value={productRaiting} readOnly />
-        </div>
-        <div>{data.reviews.length} reviews</div>
-        <div className="font-semibold"> {formatPrice(data.price)}</div>
-      </div>
-    </div>
+    <>
+      {loading ? (
+        <>
+          <Skeleton />
+        </>
+      ) : (
+        <>
+          <div
+            onClick={() => router.push(`/product/${data.id}`)}
+            className="col-span-1 cursor-pointer border-[1.2px] border-slate-200  bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm"
+          >
+            <div className="flex flex-col items-center w-full gap-1 dark:text-medium">
+              <div className="aspect-square overflow-hidden relative w-full">
+                <Image
+                  src={data.images[0].image}
+                  fill
+                  className="w-full h-full object-contain"
+                  alt={data.name}
+                />
+              </div>
+              <hr className="w-[100%] mt-6" />
+              <div className="mt-4">{truncateText(data.name)}</div>
+              <div>
+                <Rating value={productRaiting} readOnly />
+              </div>
+              <div>{data.reviews.length} reviews</div>
+              <div className="font-semibold"> {formatPrice(data.price)}</div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
